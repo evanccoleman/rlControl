884b0bba02ca53cd557f58b829109b5b42f73e73
oliviab <ofbuchanan@icloud.com>
2026-03-16 13:48:58 -0400

    Changed how graphit.py formats graphs. Graphed results of HalfCheetah-v5 in MDP/POMDP from over break

--- current/graphit.py
+++ current/graphit.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import argparse
 import sys
 from argparse import Namespace
+import matplotlib.ticker as ticker
 
 def read_command(argv) -> Namespace:
     """
@@ -55,21 +56,16 @@ for label, dirname in config['runs'].items():
     avgs_path = os.path.join(runs_dir, dirname, 'cross_agent_avgs.txt')
     with open(avgs_path) as f:
         values = [float(line.strip()) for line in f if line.strip()]
-    plt.plot(steps[:len(values)], values, marker='o', label=label)
+    plt.plot(steps[:len(values)], values, label=label)
 
 # format the plot
 plt.title(config['title'])
 plt.xlabel('Number of Steps')
 plt.ylabel('Average Rewards')
-labels = [str(s) if i % 5 == 0 or i == len(steps) - 1 else '' for i, s in enumerate(steps)]
-major = [i for i, l in enumerate(labels) if l]
-minor = [i for i, l in enumerate(labels) if not l]
+plt.ylim((-1000, 2500))
 ax = plt.gca()
-ax.set_xticks([steps[i] for i in major])
-ax.set_xticklabels([labels[i] for i in major], rotation=300)
-ax.set_xticks([steps[i] for i in minor], minor=True)
-ax.tick_params(axis='x', which='major', length=8)
-ax.tick_params(axis='x', which='minor', length=4)
-plt.legend()
+ax.xaxis.set_major_locator(ticker.MaxNLocator(nbins=11, integer=True))
+ax.tick_params(axis='x', rotation=45)
+plt.legend(loc="upper left")
 plt.tight_layout()
 plt.savefig(os.path.join(graphs_dir, config['output_filename']), dpi=150)
