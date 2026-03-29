982bea36193d295e544cf56e637a097f1d2e76a8
oliviab <ofbuchanan@icloud.com>
2026-03-28 22:49:43 -0400

    params_to_samply.py now samples from a pre-defined list of values and only optimizes three parameters per agent

--- tuning/params_to_sample.py
+++ tuning/params_to_sample.py
@@ -7,19 +7,16 @@ def sample_ppo_params(trial: optuna.Trial) -> dict:
     Sample some hyperparameters for PPO agent.
     """
 
-    learning_rate = trial.suggest_float("learning_rate", 1e-6, 1e-1, log=True)
-    ent_coef = trial.suggest_float("ent_coef", 0.00001, 0.1, log=True)
-    n_steps = 2 ** trial.suggest_int("n_steps_exponent", 3, 11)
-
-    # Note: since n_env is always 1, the rollout buffer size == n_steps
-    
-    # check for rollout buffer size and batch size compatibility
-    if n_steps % batch_size != 0:
-        raise optuna.exceptions.TrialPruned()
+    learning_rate = trial.suggest_cateogrical("learning_rate",
+                                              [0.0001, 0.001, 0.01])
+    n_steps = trial.suggest_categorical("n_steps",
+                                        [1024, 2048, 4096])
+    clip_range = trial.suggest_categorical("clip_range",
+                                           [0.1, 0.2, 0.3])
 
     return {"learning_rate": learning_rate,
-            "ent_coef": ent_coef,
             "n_steps": n_steps,
+            "clip_range": clip_range,
             }
 
 def sample_ddpg_params(trial: optuna.Trial) -> dict:
@@ -27,9 +24,12 @@ def sample_ddpg_params(trial: optuna.Trial) -> dict:
     Sample some hyperparameters for DDPG agent.
     """
 
-    learning_rate = trial.suggest_float("learning_rate", 1e-6, 1e-1, log=True)
-    batch_size = trial.suggest_categorical("batch_size", [64, 128, 256, 512, 1024])
-    tau = trial.suggest_float("tau", 0.001, 0.01, log=True)
+    learning_rate = trial.suggest_categorical("learning_rate",
+                                              [0.0001, 0.001, 0.01])
+    batch_size = trial.suggest_categorical("batch_size",
+                                           [50, 100, 150])
+    tau = trial.suggest_categorical("tau",
+                                    [0.0005, 0.005, 0.05])
 
     return {"learning_rate": learning_rate,
             "batch_size": batch_size,
@@ -40,10 +40,12 @@ def sample_td3_params(trial: optuna.Trial) -> dict:
     """
     Sample some hyperparameters for TD3 agent.
     """
-    
-    learning_rate = trial.suggest_float("learning_rate", 1e-6, 1e-1, log=True)
-    batch_size = trial.suggest_categorical("batch_size", [64, 128, 256, 512, 1024])
-    tau = trial.suggest_float("tau", 0.001, 0.01, log=True)
+    learning_rate = trial.suggest_categorical("learning_rate",
+                                              [0.0001, 0.001, 0.01])   
+    batch_size = trial.suggest_categorical("batch_size",
+                                           [50, 100, 150])
+    tau = trial.suggest_categorical("tau",
+                                    [0.0005, 0.005, 0.05])
 
     return {"learning_rate": learning_rate,
             "batch_size": batch_size,
@@ -55,9 +57,12 @@ def sample_sac_params(trial: optuna.Trial) -> dict:
     Sample some hyperparameters for SAC agent.
     """
 
-    learning_rate = trial.suggest_float("learning_rate", 1e-6, 1e-1, log=True)
-    batch_size = trial.suggest_categorical("batch_size", [64, 128, 256, 512, 1024])
-    tau = trial.suggest_float("tau", 0.001, 0.01, log=True)
+    learning_rate = trial.suggest_categorical("learning_rate",
+                                              [0.0001, 0.001, 0.01])
+    batch_size = trial.suggest_categorical("batch_size",
+                                           [50, 100, 150])
+    tau = trial.suggest_categorical("tau",
+                                    [0.0005, 0.005, 0.05])
 
     return {"learning_rate": learning_rate,
             "batch_size": batch_size,
@@ -69,19 +74,16 @@ def sample_rppo_params(trial: optuna.Trial) -> dict:
     Sample some hyperparameters for RPPO agent.
     """
 
-    learning_rate = trial.suggest_float("learning_rate", 1e-6, 1e-1, log=True)
-    ent_coef = trial.suggest_float("ent_coef", 0.00001, 0.1, log=True)
-    n_steps = 2 ** trial.suggest_int("n_steps_exponent", 3, 11)
-
-    # Note: since n_env is always 1, the rollout buffer size == n_steps
-    
-    # check for rollout buffer size and batch size compatibility
-    if n_steps % batch_size != 0:
-        raise optuna.exceptions.TrialPruned()
+    learning_rate = trial.suggest_cateogrical("learning_rate",
+                                              [0.0001, 0.001, 0.01])
+    n_steps = trial.suggest_categorical("n_steps",
+                                        [1024, 2048, 4096])
+    clip_range = trial.suggest_categorical("clip_range",
+                                           [0.1, 0.2, 0.3])
 
     return {"learning_rate": learning_rate,
-            "ent_coef": ent_coef,
             "n_steps": n_steps,
+            "clip_range": clip_range,
             }
 
 def sample_action_noise_sigma(trial: optuna.Trial) -> float:
@@ -89,4 +91,5 @@ def sample_action_noise_sigma(trial: optuna.Trial) -> float:
     Sample the sigma for action noise in off-policy algorithms,
     """
 
-    return trial.suggest_float("action_noise_sigma", 0.01, 0.3, log=False)
+    return trial.suggest_categorical("action_noise_sigma",
+                                     [0.05, 0.1, 0.15])
